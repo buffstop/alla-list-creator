d4bc41ef3e7ec7eb4b5be57dce18f53870fb5bbe
Andreas Buff <andreas.buff@buffhaltestelle.de>
2016-09-28 15:35:42 +0200

    initial commit

---
+++ AllaListCreator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AllaListCreator
+//
+//  Created by Andreas Buff on 28/09/16.
+//  Copyright © 2016 buffhaltestelle. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
